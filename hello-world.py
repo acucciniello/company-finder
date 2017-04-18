10852ad32b43e6f03a23ce4abb91f2f12ac633d4
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-04-17 22:09:38 -0400

    Pull from Glassdoor API
        Data is now JSON and serializable.
        TODO: Implement the method I have drawn out for finding the last
        companies on the list

--- hello-world.py
+++ hello-world.py
@@ -1,13 +1,32 @@
 import tornado.ioloop
 import tornado.web
+import json
+from tornado.httpclient import AsyncHTTPClient
+
 
 class MainHandler(tornado.web.RequestHandler):
+
+  def handle_response(self, response):
+    if response.error:
+      print('Error: %s' % response.error)
+    else:
+      stringOutput = response.body.decode('utf-8')
+      output = json.loads(stringOutput)
+      print(output['response']['employers'])
+
+  # Maybe what I need to do here is check how many pages there are and from there
+  # We can make a second request to pull the last page's worth of data
+
+
   def get(self):
-    self.write("Hello World")
+    print('We are here')
+    http_client = AsyncHTTPClient()
+    http_client.fetch('http://api.glassdoor.com/api/api.htm?t.p=142069&t.k=dsrY9os4nMQ&userip=0.0.0.0&useragent=&format=json&v=1&action=employers', self.handle_response)
+
 
 def make_app():
   return tornado.web.Application([
-    (r'/', MainHandler),
+    (r'/', MainHandler)
   ])
 
 if __name__ == "__main__":
